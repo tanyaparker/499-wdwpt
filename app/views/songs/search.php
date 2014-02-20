4141e90a433995ebd20811ab21a34535444e4c21
Tanya Parker <tanyacparker@gmail.com>
2014-02-19 16:39:03 -0800

    moving laravel project to GitHub

---
+++ app/views/songs/search.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+	<title>Song Search</title>
+</head>
+
+<body>
+
+<form method="get" action="/songs">
+	<h1>Song Search</h1>
+	<div>
+		Song Title:
+		<input type="text" name="title" />
+	</div>
+
+	<div>
+		Artist:
+		<input type="text" name="artist" />
+	</div>
+
+	<div>
+		<input type="submit"  value="Search" />
+	</div>
+</form>
+
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,1 @@
+## ITP 499 Homework Submissions
