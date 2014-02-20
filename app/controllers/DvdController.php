f8908d59055e2da5ef28028398dbac9069c2db15
Tanya Parker <tanyacparker@gmail.com>
2014-02-19 17:33:58 -0800

    DVD controller

---
+++ app/controllers/DvdController.php
@@ -0,0 +1,22 @@
+<?php
+
+class DvdController {
+
+	public function search()
+	{
+		return View::make('dvds/search');
+	}
+
+	public function listDvds()
+	{
+		$title = Input::get('title');
+		$genre = Input::get('genre');
+		$rating = Input::get('rating');
+		$dvds = Dvd::search($title, $genre, $rating);	
+
+		return View::make('dvds/dvds-list', [
+			'dvds' => $dvds
+		]);
+	}
+
+}
