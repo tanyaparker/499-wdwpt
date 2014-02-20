cddcbc1fabdadfc55fe1685a98ec4230c3c5a74a
Tanya Parker <tanyacparker@gmail.com>
2014-02-19 16:45:07 -0800

    basic search and results

---
+++ app/views/dvds/dvds-list.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+	<title>DVD Results</title>
+</head>
+
+<body>
+	<h1>Your search returned: </h1>
+
+	<?php foreach($dvds as $dvd) : ?>
+		<div class="dvd">
+			<h4>
+				<?php echo $dvd->title; ?> by
+			</h4>
+			<p>Rating: <?php echo $dvd->rating; ?></p>
+			<p>Genre: <?php echo $dvd->genre; ?></p>
+			<p>Label: <?php echo $dvd->label; ?></p>
+			<p>Format: <?php echo $dvd->format; ?></p>
+			<p>Sound: <?php echo $dvd->sound; ?></p>
+		</div>
+	<?php endforeach; ?>
+
+
+</body>
+</html>
---
+++ app/views/dvds/search/search.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+	<title>DVD Search</title>
+</head>
+
+<body>
+
+<form method="get" action="/dvds">
+	<h1>DVD Search</h1>
+	<div>
+		DVD Title:
+		<input type="text" name="title" />
+	</div>
+	<div>
+		<input type="submit"  value="Search" />
+	</div>
+</form>
+
+
+</body>
+</html>
