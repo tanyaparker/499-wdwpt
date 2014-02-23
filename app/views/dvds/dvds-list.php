1ce9c4a75ae6dbcc342da7111997dc70e3ac0fbb
Tanya Parker <tanyacparker@gmail.com>
2014-02-22 22:28:26 -0800

    changed to table, added error if none found

--- app/views/dvds/dvds-list.php
+++ app/views/dvds/dvds-list.php
@@ -9,18 +9,38 @@
 <body>
 	<h1>Your search returned: </h1>
 
-	<?php foreach($dvds as $dvd) : ?>
-		<div class="gradientBox">
-			<b><u><?php echo $dvd->title; ?><br /></b></u>
-			Rating: <?php echo $dvd->rating_name; ?><br />
-			Genre: <?php echo $dvd->genre_name; ?><br />
-			Label: <?php echo $dvd->label_name; ?><br />
-			Sound: <?php echo $dvd->sound_name; ?><br />
-			Format: <?php echo $dvd->format_name; ?><br />
-			Release Data: <?php echo $dvd->release_date; ?><br />
-		</div>
-	<?php endforeach; ?>
+	<?php if(sizeof($dvds) >= 1) {?>
+<table>
 
+	<tr style="font-size: 20px;">
+		<td><b><u>Title</u></b></td>
+		<td><b><u>Rating</u></b></td>
+		<td><b><u>Genre</u></b></td>
+		<td><b><u>Label</u></b></td>
+		<td><b><u>Sound</u></b></td>
+		<td><b><u>Format</u></b></td>
+		<td><b><u>Release Date</b></u></td>
+	</tr>
+	<?php foreach($dvds as $dvd) : ?>
+
+		<tr>
+			<td><?php echo $dvd->title; ?></td>
+			<td><?php echo $dvd->rating_name; ?></td>
+			<td><?php echo $dvd->genre_name; ?></td>
+			<td><?php echo $dvd->label_name; ?></td>
+			<td><?php echo $dvd->sound_name; ?></td>
+			<td><?php echo $dvd->format_name; ?></td>
+			<td><?php echo $dvd->release_date; ?></td>
+		</tr>
+		
+	<?php endforeach; ?>
+</table>
+<?php
+}
+else {
+	echo "<h1>nothing. Try again!</h1>";
+}
+?>
 
 </body>
 </html>
